7331e1f7fbdf2d81c7f2551208c806697a374c58
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-02-23 19:14:02 -0300

    ajustes de responsividade e layout

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -6,25 +6,26 @@ import './style/navmenu.css'
 function NavMenu() {
     return (
     <Navbar className="NavBar" bg="dark" variant="dark">
-
         <Container className="NavBar-Container">
           <Row xs={1} md={4}>
             <Navbar.Brand href="#home" >
-              <img alt="" src={Logo} width="30" height="30" className="d-inline-block align-top"/>
+              <img alt="" src={Logo} className="logo d-inline-block align-top"/>
               <spam className="NavBar-Logo">Projeto Marvel</spam>
             </Navbar.Brand>
-            <Nav defaultActiveKey="/home" as="ul">
-              <Nav.Item as="li">
-                <Nav.Link href="/home">Home</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-1">Novidades</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-2">Sobre</Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Form className="d-flex" md="auto">
+            <div className='align-center'>
+              <Nav defaultActiveKey="/home" as="ul">
+                <Nav.Item as="li">
+                  <Nav.Link href="/home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                  <Nav.Link eventKey="link-1">Novidades</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                  <Nav.Link eventKey="link-2">Sobre</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </div>
+            <Form className="d-flex align-center" md="auto">
               <FormControl type="search" placeholder="Search" className="me-2" aria-label="Procurar" style={{width: "auto"}}/>
               <Button variant="dark">Busca</Button>
             </Form>
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import '../components/style/home.css';
+import Topo from '../components/Topo';
+import Corpo from '../components/Corpo';
+import Slider from '../components/Slider';
+import Rodape from '../components/Rodape';
+
+
+
+function Home() {
+  return (
+    <div className="App">
+      <Topo/>
+      <Slider/>
+      <Corpo/>
+      <Rodape/>
+    </div>
+  );
+}
+
+export default Home;
