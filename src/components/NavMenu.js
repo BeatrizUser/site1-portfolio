94457825e5590b8e23cb900ac3911c739dd226d6
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-03-20 02:40:58 -0300

    Finalização Definitiva.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -7,16 +7,16 @@ function NavMenu() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">
+          <span href="#home">
             <img alt="" src={Logo} className="logo d-inline-block align-top" />
             <span className="NavBar-Logo">Projeto Marvel</span>
-          </Navbar.Brand>
+          </span>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className='menu-animado' href="/home">Home</Nav.Link>
-              <Nav.Link className='menu-animado' eventKey="link-1">Novidades</Nav.Link>
-              <Nav.Link className='menu-animado' eventKey="link-2">Sobre</Nav.Link>               
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link eventKey="link-1">Novidades</Nav.Link>
+              <Nav.Link eventKey="link-2">Sobre</Nav.Link>               
             </Nav>
               <Form className="d-flex align-center" md="auto">
                 <FormControl type="search" placeholder="Search" className="me-2" aria-label="Procurar" style={{ width: "50vw"}} />
--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -6,7 +6,8 @@ function Rodape(){
     return(
       <footer className="footer-copyright">
         <div className="footer-copyright">
-          &copy; {new Date().getFullYear()} Copyright: <strong>Desenvolvido por</strong> <a href="https://github.com/BeatrizUser">Beatriz Machado</a> 
+          <span>&copy; {new Date().getFullYear()} Copyright:</span>
+          <span className="span-rodape"><strong>Desenvolvido por</strong> <a href="https://github.com/BeatrizUser">Beatriz Machado</a></span> 
         </div>
       </footer>
   )
